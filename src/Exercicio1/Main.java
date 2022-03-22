981194090d5890b0c4d0814e5d83f289cff434b2
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2022-03-22 17:18:22 -0300

    DesafioGftExer1

---
+++ src/Exercicio1/Main.java
@@ -0,0 +1,21 @@
+package Exercicio1;
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        NumerosImpares numerosImpares = new NumerosImpares();
+
+
+        numerosImpares.setNumInicial(1);
+        numerosImpares.setNumFinal(10);
+
+        numerosImpares.mostrarNumeros();
+
+
+    }
+}
---
+++ src/Exercicio1/NumerosImpares.java
@@ -0,0 +1,51 @@
+package Exercicio1;
+
+public class NumerosImpares {
+
+    private int numInicial;
+    private int numFinal;
+
+
+    public int mostrarNumeros() {
+        int i =0;
+
+
+
+            for (i = numInicial; i < numFinal; i++) {
+
+                if (i % 2 == 1) {
+
+                    System.out.print(i + " ");
+
+                }
+
+            }
+
+
+        return i ;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "numInicial=" + numInicial +
+                ", numFinal=" + numFinal +
+                '}';
+    }
+
+    public int getNumInicial() {
+        return numInicial;
+    }
+
+    public void setNumInicial(int numInicial) {
+        this.numInicial = numInicial;
+    }
+
+    public int getNumFinal() {
+        return numFinal;
+    }
+
+    public void setNumFinal(int numFinal) {
+        this.numFinal = numFinal;
+    }
+}
